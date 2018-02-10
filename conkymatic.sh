13431e8dbf533146ebd39172a974c318aefa4480
rickellis <rickellis@gmail.com>
2018-02-09 17:45:17 -0700

    .

--- conkymatic.sh
+++ conkymatic.sh
@@ -39,9 +39,6 @@ COLORPIMG="colorpalette.png"
 # Desired width of color palette image
 COLORPWIDTH="224"
 
-# Number of colors in the palette image
-COLORPN="16"
-
 # Default background color
 COLOR_BG="#000000"
 
@@ -128,13 +125,13 @@ fi
 # WELCOME MESSAGE ---------------------------------------------------------------------
 
 echo ""
-echo "--------------------------------------------------------------------"
-echo "           Welcome to ConkyMatic Version ${VERSION}"
-echo "--------------------------------------------------------------------"
+echo "-----------------------------------------------------------------------"
+echo "             Welcome to ConkyMatic Version ${VERSION}"
+echo "-----------------------------------------------------------------------"
 
 echo ""
 echo "Please select MODE:"
-echo "1) Automatic. The colors will be auto-selected from the wallpaper colors."
+echo "1) Automatic. Colors will be auto-selected from current wallpaper."
 echo "2) Manual. You will be prompted to enter each color hex value."
 read MODE
 
@@ -423,6 +420,9 @@ fi
 
 # DOWNLOAD THE WEATHER JSON FILE --------------------------------------------------------
 
+echo ""
+echo "Downloading weather data"
+
 # Curl argumnets:
 #   -f = fail silently. Will issue error code 22
 #   -k = Non-strict SSL connection
@@ -437,59 +437,187 @@ echo "${CURL}" > ${CACHEDIR}/${JSON_CACHEFILE}
 
 
 
-# GENERATE COLORPALETTE PNG -------------------------------------------------------------
+# GENERATE COLOR PALETTE PNG ------------------------------------------------------------
 
 echo ""
-echo "Generating colorpalette image."
+echo "Exporting color palette image"
 
 # Use ImageMagick to create a color palette image based on the current wallpaper
 convert ${WALLPAPERPATH} \
 +dither \
--colors ${COLORPN} \
+-colors 16 \
 -unique-colors \
 -filter box \
 -geometry ${COLORPWIDTH} \
 ${CACHEDIR}/${COLORPIMG}
 
+
+# GENERATE AUTOMATIC COLORS -------------------------------------------------------------
+
 # If the user selected automatic mode we need to grab the hex values from the colorpalette PNG.
 if [[ ${MODE} -eq 1 ]]; then
 
-    palette_image="${CACHEDIR}/${COLORPIMG}"
+    echo ""
+    echo "Extracting hex values from color palette image"
 
-    # Create a text file with the color values
-    convert ${palette_image} -format %c -depth 8  histogram:info:/tmp/colorpalette.txt
+    # Create a micro version of the color palette PNG: 1px x 16px 
+    MICROIMG="${CACHEDIR}/micropalette.png"
 
-    # Extract hex color values
-    sed -r 's/.*[[:space:]](\#[a-zA-Z0-9]+)[[:space:]].*/\1/w /tmp/hexcolors.txt' /tmp/colorpalette.txt > /tmp/hexcolors.txt;
+    convert ${CACHEDIR}/${COLORPIMG} \
+    -colors 16 \
+    -unique-colors \
+    -depth 8 \
+    -size 1x16 \
+    -geometry 16 \
+    ${MICROIMG}
 
-    readarray -t COLARRAY </tmp/hexcolors.txt
 
-    # Set the variables
+    # EXTRACT COLOR VAlUES --------------------------------------------------------------
 
-    # Background color
-    COLOR_BG="${COLARRAY[2]}"
+    COLOR1=$(convert ${MICROIMG} -crop '1x1+0+0' txt:-)
+    # Remove newlines 
+    COLOR1=${COLOR1//$'\n'/}
+    # Extract the hex color value
+    COLARRAY[1]=$(echo "$COLOR1" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+
+    COLOR2=$(convert ${MICROIMG} -crop '1x1+1+0' txt:-)
+    # Remove newlines 
+    COLOR2=${COLOR2//$'\n'/}
+    # Extract the hex color value
+    COLARRAY[2]=$(echo "$COLOR2" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+
+    COLOR3=$(convert ${MICROIMG} -crop '1x1+2+0' txt:-)
+    # Remove newlines 
+    COLOR3=${COLOR3//$'\n'/}
+    # Extract the hex color value
+    COLARRAY[3]=$(echo "$COLOR3" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+
+    COLOR4=$(convert ${MICROIMG} -crop '1x1+3+0' txt:-)
+    # Remove newlines 
+    COLOR4=${COLOR4//$'\n'/}
+    # Extract the hex color value
+    COLARRAY[4]=$(echo "$COLOR4" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+
+    COLOR5=$(convert ${MICROIMG} -crop '1x1+4+0' txt:-)
+    # Remove newlines 
+    COLOR5=${COLOR5//$'\n'/}
+    # Extract the hex color value
+    COLARRAY[5]=$(echo "$COLOR5" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+
+    COLOR6=$(convert ${MICROIMG} -crop '1x1+5+0' txt:-)
+    # Remove newlines 
+    COLOR6=${COLOR6//$'\n'/}
+    # Extract the hex color value
+    COLARRAY[6]=$(echo "$COLOR6" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+
+    COLOR7=$(convert ${MICROIMG} -crop '1x1+6+0' txt:-)
+    # Remove newlines 
+    COLOR7=${COLOR7//$'\n'/}
+    # Extract the hex color value
+    COLARRAY[7]=$(echo "$COLOR7" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+
+    COLOR8=$(convert ${MICROIMG} -crop '1x1+7+0' txt:-)
+    # Remove newlines 
+    COLOR8=${COLOR8//$'\n'/}
+    # Extract the hex color value
+    COLARRAY[8]=$(echo "$COLOR8" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+
+    COLOR9=$(convert ${MICROIMG} -crop '1x1+8+0' txt:-)
+    # Remove newlines 
+    COLOR9=${COLOR9//$'\n'/}
+    # Extract the hex color value
+    COLARRAY[9]=$(echo "$COLOR9" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+
+    COLOR10=$(convert ${MICROIMG} -crop '1x1+9+0' txt:-)
+    # Remove newlines 
+    COLOR10=${COLOR10//$'\n'/}
+    # Extract the hex color value
+    COLARRAY[10]=$(echo "$COLOR10" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+
+    COLOR11=$(convert ${MICROIMG} -crop '1x1+10+0' txt:-)
+    # Remove newlines 
+    COLOR11=${COLOR11//$'\n'/}
+    # Extract the hex color value
+    COLARRAY[11]=$(echo "$COLOR11" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+
+    COLOR12=$(convert ${MICROIMG} -crop '1x1+11+0' txt:-)
+    # Remove newlines 
+    COLOR12=${COLOR12//$'\n'/}
+    # Extract the hex color value
+    COLARRAY[12]=$(echo "$COLOR12" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+
+    COLOR13=$(convert ${MICROIMG} -crop '1x1+12+0' txt:-)
+    # Remove newlines 
+    COLOR13=${COLOR13//$'\n'/}
+    # Extract the hex color value
+    COLARRAY[13]=$(echo "$COLOR13" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+
+    COLOR14=$(convert ${MICROIMG} -crop '1x1+13+0' txt:-)
+    # Remove newlines 
+    COLOR14=${COLOR14//$'\n'/}
+    # Extract the hex color value
+    COLARRAY[14]=$(echo "$COLOR14" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+
+    COLOR15=$(convert ${MICROIMG} -crop '1x1+14+0' txt:-)
+    # Remove newlines 
+    COLOR15=${COLOR15//$'\n'/}
+    # Extract the hex color value
+    COLARRAY[15]=$(echo "$COLOR15" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+
+    COLOR16=$(convert ${MICROIMG} -crop '1x1+15+0' txt:-)
+    # Remove newlines 
+    COLOR16=${COLOR16//$'\n'/}
+    # Extract the hex color value
+    COLARRAY[16]=$(echo "$COLOR16" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+
+
+    # Delete micro image
+    rm ${MICROIMG}
+
+    # SET COLOR VARIABLES ---------------------------------------------------------------
+
+    echo ""
+    echo "Building color map"
+
+    # Background color, randomly selected from the 3 darkest colors
+    bgn=$(shuf -i 1-3 -n 1)
+    COLOR_BG="${COLARRAY[${bgn}]}"
 
     # Border color
-    COLOR_BORDER="${COLARRAY[11]}"
+    COLOR_BORDER="${COLARRAY[12]}"
 
     # Weather icon color
-    COLOR_ICON="${COLARRAY[13]}"
-
-    # Primary text color
-    COLOR_PRIMARY="${COLARRAY[14]}"
-
-    # Data text color
-    COLOR_DATA="${COLARRAY[13]}"
-
-    # Accent text color
-    COLOR_ACCENT="${COLARRAY[12]}"
+    COLOR_ICON="${COLARRAY[14]}"
 
     # HR color
-    COLOR_HR="${COLARRAY[11]}"
+    COLOR_HR="${COLARRAY[10]}"
 
-    # Battery bar color
-    COLOR_BAT_FULL="${COLARRAY[12]}"
+    # Bars normal
+    COLOR_BARS_NORM="${COLARRAY[12]}"
 
+    # Bars warning
+    COLOR_BARS_WARN="${COLARRAY[11]}"
+
+    # Time color
+    COLOR_TIME="${COLARRAY[16]}"
+
+    # Date color
+    COLOR_DATE="${COLARRAY[15]}"
+
+    # Weather data color
+    COLOR_WEATHER="${COLARRAY[13]}"
+
+    # Headings
+    COLOR_HEADINGS="${COLARRAY[16]}"
+
+    # Subheadings
+    COLOR_SUBHEADINGS="${COLARRAY[13]}"
+
+    # Data values
+    COLOR_DATA="${COLARRAY[10]}"
+
+    # Color extra
+    COLOR_EXTRA="${COLARRAY[7]}"
 
 fi 
 
@@ -516,9 +644,12 @@ done
 # CONVERT SVG TO PNG  -------------------------------------------------------------------
 
 echo ""
-echo "Converting images using: $converter"
+echo "Exporting weather icons using $converter"
 echo ""
 
+
+str="."
+i=1
 for filepath in "${TEMPDIR}"/*.svg
     do
 
@@ -528,7 +659,7 @@ for filepath in "${TEMPDIR}"/*.svg
     # Remove the file extension, leaving only the name  
     name="${filename%%.*}"
 
-    echo "Converting ${filename}"
+   # echo "Converting ${filename}"
 
     # Convert to PNG using either Inkscape or ImageMagick
     if [[ $converter == "inkscape" ]]
@@ -557,6 +688,9 @@ for filepath in "${TEMPDIR}"/*.svg
                             }
     fi
 
+    echo -ne "(Exporting image ${i}) ${str}\r"
+    ((i++))
+    str="${str}."
 done
 
 # Delete the temporary directory
@@ -583,10 +717,18 @@ cp -f ${PNGICONS}/${FORECASTCODE5}.png ${CACHEDIR}/forecast5.png
 
 
 
+# KILL CONKY ----------------------------------------------------------------------------
+
+echo ""
+echo ""
+echo "Closing Conky"
+pkill conky
+
+
 # REPLACE TEMPLATE VARIABLES ------------------------------------------------------------
 
 echo ""
-echo "Building conkyrc file based on ${TEMPLFILE}"
+echo "Exporting new conkyrc file"
 
 # Before replacing vars make a copy of the template
 cp ${TEMPLDIR}/${TEMPLFILE} ${CACHEDIR}/conkyrc
@@ -613,35 +755,30 @@ sed -i -e "s|_VAR:BG_TRANS_|${BG_TRANS}|g" "${CACHEDIR}/conkyrc"
 
 # Colors
 sed -i -e "s|_VAR:COLOR_BG_|${COLOR_BG}|g" "${CACHEDIR}/conkyrc"
-sed -i -e "s|_VAR:COLOR_PRIMARY_|${COLOR_PRIMARY}|g" "${CACHEDIR}/conkyrc"
-sed -i -e "s|_VAR:COLOR_DATA_|${COLOR_DATA}|g" "${CACHEDIR}/conkyrc"
-sed -i -e "s|_VAR:COLOR_ACCENT_|${COLOR_ACCENT}|g" "${CACHEDIR}/conkyrc"
 sed -i -e "s|_VAR:COLOR_HR_|${COLOR_HR}|g" "${CACHEDIR}/conkyrc"
-sed -i -e "s|_VAR:COLOR_BAT_FULL_|${COLOR_BAT_FULL}|g" "${CACHEDIR}/conkyrc"
-sed -i -e "s|_VAR:COLOR_BAT_EMPT_|${COLOR_BAT_EMPT}|g" "${CACHEDIR}/conkyrc"
+sed -i -e "s|_VAR:COLOR_BARS_NORM_|${COLOR_BARS_NORM}|g" "${CACHEDIR}/conkyrc"
+sed -i -e "s|_VAR:COLOR_BARS_WARN_|${COLOR_BARS_WARN}|g" "${CACHEDIR}/conkyrc"
 sed -i -e "s|_VAR:COLOR_BORDER_|${COLOR_BORDER}|g" "${CACHEDIR}/conkyrc"
+sed -i -e "s|_VAR:COLOR_TIME_|${COLOR_TIME}|g" "${CACHEDIR}/conkyrc"
+sed -i -e "s|_VAR:COLOR_DATE_|${COLOR_DATE}|g" "${CACHEDIR}/conkyrc"
+sed -i -e "s|_VAR:COLOR_WEATHER_|${COLOR_WEATHER}|g" "${CACHEDIR}/conkyrc"
+sed -i -e "s|_VAR:COLOR_HEADINGS_|${COLOR_HEADINGS}|g" "${CACHEDIR}/conkyrc"
+sed -i -e "s|_VAR:COLOR_SUBHEADINGS_|${COLOR_SUBHEADINGS}|g" "${CACHEDIR}/conkyrc"
+sed -i -e "s|_VAR:COLOR_DATA_|${COLOR_DATA}|g" "${CACHEDIR}/conkyrc"
+sed -i -e "s|_VAR:COLOR_EXTRA_|${COLOR_EXTRA}|g" "${CACHEDIR}/conkyrc"
 
 
 # CLEANUP -------------------------------------------------------------------------------
-
-# Kill conky
-echo ""
-echo "Shutting down conky."
-pkill conky
     
 # Copy conkyrc file to its proper location
-echo ""
-echo "Copying new .conkyrc file to home directory."
 cp ${CACHEDIR}/conkyrc ~/.conkyrc
 
 # Remove the temp file
-echo ""
-echo "Deleting temporary .conkyrc file"
 rm ${CACHEDIR}/conkyrc
 
 # Launch conky
 echo ""
-echo "Relaunching conky."
+echo "Relaunching Conky"
 conky 2>/dev/null
 
 echo ""
