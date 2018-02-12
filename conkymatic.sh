5c039d63e18f14c3ad34e30356cce5cc49a5c2a0
rickellis <rickellis@gmail.com>
2018-02-11 17:07:27 -0700

    Improved error trapping

--- conkymatic.sh
+++ conkymatic.sh
@@ -199,19 +199,20 @@ fi
 
 # SELECT A TEMPLATE ---------------------------------------------------------------------
 
-# If there is only one template there's no reason to have them select it
+# If there is only one template we auto-select it
 if [[ ${TMPLN} == 1 ]]; then
         
     # Set the template
     TEMPLATE_SELECTION="${TMPL_ARRAY[0]}"
 
-    # Multiple templates
-else
+else  # Multiple templates in the Templates folder
+
     echo ""
     echo " Please select the template NUMBER you would like to use."
     echo " Or hit ENTER to select default.conky:"
 
     i=1
+    # Display a list of templates
     for tmpl in ${TMPL_ARRAY[@]}; do
         filename="${tmpl##*/}"
         echo " [${i}] $filename"
@@ -220,7 +221,7 @@ else
 
     read CHOICE
 
-    # If they hit enter we use the default
+    # If they hit enter we use default.conky
     if [[ -z ${CHOICE} ]]; then
 
         TEMPLATE_SELECTION="${TEMPLATE_DIRECTORY}/default.conky"
@@ -234,7 +235,7 @@ else
             echo ""
             exit 1
         fi
-    else # They entered a value
+    else  # The user chose a specific template
 
         # Did they enter an integer?
         if [[ $CHOICE =~ ^?[0-9]+$ ]]; then
@@ -282,7 +283,7 @@ echo "${CURL}" > ${CACHE_DIRECTORY}/${JSON_CACHE_FILE}
 # GENERATE COLOR PALETTE PNG ------------------------------------------------------------
 
 echo ""
-echo " Exporting color palette based on the wallpaper's 16 most common colors"
+echo " Generating color palette based on the wallpaper's 16 most common colors"
 
 # Use ImageMagick to create a color palette image based on the current wallpaper
 convert ${WALLPAPER_PATH} \
@@ -319,103 +320,136 @@ ${MICROIMG}
 # the color palette image is created. I ended up having to extract each color value 
 # based on the x/y coordinates of the micropalette image. At some point I'd like to
 # revisit this code to see if I can find a more graceful way to accomplish this.
+# Also, some images have alpha tranparencies, so we end up with 9 character hex
+# values. We truncate all color values at 7.
 
 COLOR1=$(convert ${MICROIMG} -crop '1x1+0+0' txt:-)
 # Remove newlines 
 COLOR1=${COLOR1//$'\n'/}
 # Extract the hex color value
-COLARRAY[1]=$(echo "$COLOR1" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+COLOR1=$(echo "$COLOR1" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+# Truncate hex value at 7 characters. 
+COLARRAY[1]="${COLOR1:0:7}"
 
 COLOR2=$(convert ${MICROIMG} -crop '1x1+1+0' txt:-)
 # Remove newlines 
 COLOR2=${COLOR2//$'\n'/}
 # Extract the hex color value
-COLARRAY[2]=$(echo "$COLOR2" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+COLOR2=$(echo "$COLOR2" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+# Truncate hex value at 7 characters. 
+COLARRAY[2]="${COLOR2:0:7}"
 
 COLOR3=$(convert ${MICROIMG} -crop '1x1+2+0' txt:-)
 # Remove newlines 
 COLOR3=${COLOR3//$'\n'/}
 # Extract the hex color value
-COLARRAY[3]=$(echo "$COLOR3" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+COLOR3=$(echo "$COLOR3" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+# Truncate hex value at 7 characters. 
+COLARRAY[3]="${COLOR3:0:7}"
 
 COLOR4=$(convert ${MICROIMG} -crop '1x1+3+0' txt:-)
 # Remove newlines 
 COLOR4=${COLOR4//$'\n'/}
 # Extract the hex color value
-COLARRAY[4]=$(echo "$COLOR4" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+COLOR4=$(echo "$COLOR4" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+# Truncate hex value at 7 characters. 
+COLARRAY[4]="${COLOR4:0:7}"
 
 COLOR5=$(convert ${MICROIMG} -crop '1x1+4+0' txt:-)
 # Remove newlines 
 COLOR5=${COLOR5//$'\n'/}
 # Extract the hex color value
-COLARRAY[5]=$(echo "$COLOR5" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+COLOR5=$(echo "$COLOR5" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+# Truncate hex value at 7 characters. 
+COLARRAY[5]="${COLOR5:0:7}"
 
 COLOR6=$(convert ${MICROIMG} -crop '1x1+5+0' txt:-)
 # Remove newlines 
 COLOR6=${COLOR6//$'\n'/}
 # Extract the hex color value
-COLARRAY[6]=$(echo "$COLOR6" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')Conky
+COLOR6=$(echo "$COLOR6" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+# Truncate hex value at 7 characters. 
+COLARRAY[6]="${COLOR6:0:7}"
 
 COLOR7=$(convert ${MICROIMG} -crop '1x1+6+0' txt:-)
 # Remove newlines 
 COLOR7=${COLOR7//$'\n'/}
 # Extract the hex color value
-COLARRAY[7]=$(echo "$COLOR7" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+COLOR7=$(echo "$COLOR7" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+# Truncate hex value at 7 characters. 
+COLARRAY[7]="${COLOR7:0:7}"
 
 COLOR8=$(convert ${MICROIMG} -crop '1x1+7+0' txt:-)
 # Remove newlines 
 COLOR8=${COLOR8//$'\n'/}
 # Extract the hex color value
-COLARRAY[8]=$(echo "$COLOR8" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+COLOR8=$(echo "$COLOR8" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+# Truncate hex value at 7 characters. 
+COLARRAY[8]="${COLOR8:0:7}"
 
 COLOR9=$(convert ${MICROIMG} -crop '1x1+8+0' txt:-)
 # Remove newlines 
 COLOR9=${COLOR9//$'\n'/}
 # Extract the hex color value
-COLARRAY[9]=$(echo "$COLOR9" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+COLOR9=$(echo "$COLOR9" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+# Truncate hex value at 7 characters. 
+COLARRAY[9]="${COLOR9:0:7}"
 
 COLOR10=$(convert ${MICROIMG} -crop '1x1+9+0' txt:-)
 # Remove newlines 
 COLOR10=${COLOR10//$'\n'/}
 # Extract the hex color value
-COLARRAY[10]=$(echo "$COLOR10" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+COLOR10=$(echo "$COLOR10" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+# Truncate hex value at 7 characters. 
+COLARRAY[10]="${COLOR10:0:7}"
 
 COLOR11=$(convert ${MICROIMG} -crop '1x1+10+0' txt:-)
 # Remove newlines 
 COLOR11=${COLOR11//$'\n'/}
 # Extract the hex color value
-COLARRAY[11]=$(echo "$COLOR11" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+COLOR11=$(echo "$COLOR11" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+# Truncate hex value at 7 characters. 
+COLARRAY[11]="${COLOR11:0:7}"
 
 COLOR12=$(convert ${MICROIMG} -crop '1x1+11+0' txt:-)
 # Remove newlines 
 COLOR12=${COLOR12//$'\n'/}
 # Extract the hex color value
-COLARRAY[12]=$(echo "$COLOR12" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+COLOR12=$(echo "$COLOR12" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+# Truncate hex value at 7 characters. 
+COLARRAY[12]="${COLOR12:0:7}"
 
 COLOR13=$(convert ${MICROIMG} -crop '1x1+12+0' txt:-)
 # Remove newlines 
 COLOR13=${COLOR13//$'\n'/}
 # Extract the hex color value
-COLARRAY[13]=$(echo "$COLOR13" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+COLOR13=$(echo "$COLOR13" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+# Truncate hex value at 7 characters. 
+COLARRAY[13]="${COLOR13:0:7}"
 
 COLOR14=$(convert ${MICROIMG} -crop '1x1+13+0' txt:-)
 # Remove newlines 
 COLOR14=${COLOR14//$'\n'/}
 # Extract the hex color value
-COLARRAY[14]=$(echo "$COLOR14" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+COLOR14=$(echo "$COLOR14" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+# Truncate hex value at 7 characters. 
+COLARRAY[14]="${COLOR14:0:7}"
 
 COLOR15=$(convert ${MICROIMG} -crop '1x1+14+0' txt:-)
 # Remove newlines 
 COLOR15=${COLOR15//$'\n'/}
 # Extract the hex color value
-COLARRAY[15]=$(echo "$COLOR15" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+COLOR15=$(echo "$COLOR15" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+# Truncate hex value at 7 characters. 
+COLARRAY[15]="${COLOR15:0:7}"
 
 COLOR16=$(convert ${MICROIMG} -crop '1x1+15+0' txt:-)
 # Remove newlines 
 COLOR16=${COLOR16//$'\n'/}
 # Extract the hex color value
-COLARRAY[16]=$(echo "$COLOR16" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
-
+COLOR16=$(echo "$COLOR16" | sed 's/.*[[:space:]]\(#[a-zA-Z0-9]\+\)[[:space:]].*/\1/')
+# Truncate hex value at 7 characters. 
+COLARRAY[16]="${COLOR16:0:7}"
 
 # Delete micro image
 rm ${MICROIMG}
@@ -433,51 +467,51 @@ echo " Building a randomized color map"
 RND=$(shuf -i 1-3 -n 1)
 COLOR_BACKGROUND="${COLARRAY[${RND}]}"
 
-# Border color. Select from all but the darkst 5
+# Border color. Colors 5-13
 RND=$(shuf -i 5-13 -n 1)
 COLOR_BORDER="${COLARRAY[${RND}]}"
 
-# Weather icon color. Mostly light colors
+# Weather icon color. Colors 12-16
 RND=$(shuf -i 12-16 -n 1)
 COLOR_ICON="${COLARRAY[${RND}]}"
 
-# HR color. Midrange and light colors
-RND=$(shuf -i 7-13 -n 1)
+# Horizontal rule color. Colors 5-14
+RND=$(shuf -i 5-14 -n 1)
 COLOR_HR="${COLARRAY[${RND}]}"
 
-# Bars normal. Mid high to light colors.
-RND=$(shuf -i 10-16 -n 1)
+# Bars normal. Colors 9-16
+RND=$(shuf -i 9-16 -n 1)
 COLOR_BARS_NORM="${COLARRAY[${RND}]}"
 
 # Bars warning
 # Hard code this red
 COLOR_BARS_WARN="#fc1b0f"
 
-# Time color. Mostly ligth colors
+# Time color. Colors 12-16
 RND=$(shuf -i 12-16 -n 1)
 COLOR_TIME="${COLARRAY[${RND}]}"
 
-# Date color. Mid high to light.
-RND=$(shuf -i 11-16 -n 1)
+# Date color. Colors 10-16
+RND=$(shuf -i 10-16 -n 1)
 COLOR_DATE="${COLARRAY[${RND}]}"
 
-# Weather data color. Light colors.
+# Weather data color. Colors 12-16
 RND=$(shuf -i 12-16 -n 1)
 COLOR_WEATHER="${COLARRAY[${RND}]}"
 
-# Headings. Mid high to light
+# Headings. Colors 9-16
 RND=$(shuf -i 9-16 -n 1)
 COLOR_HEADING="${COLARRAY[${RND}]}"
 
-# Subheadings. Mid high to light
-RND=$(shuf -i 9-15 -n 1)
+# Subheadings. Colors 9-16
+RND=$(shuf -i 9-16 -n 1)
 COLOR_SUBHEADING="${COLARRAY[${RND}]}"
 
-# Data values. Mid to light colors.
+# Data values. Colors 8-16
 RND=$(shuf -i 8-16 -n 1)
 COLOR_DATA="${COLARRAY[${RND}]}"
 
-# Color text. Mid to high
+# Color text. Colors 8-16
 RND=$(shuf -i 8-16 -n 1)
 COLOR_TEXT="${COLARRAY[${RND}]}"
 
@@ -558,8 +592,7 @@ rm -R "${TEMPDIR}"
 
 
 # CACHE THE CURRENT WEATHER & FORECAST ICONS --------------------------------------------
-# These are the icons that get displayed in the conky. They get regenerated
-# every five minutes via code in the conky itself
+# These are the icons that get displayed in the conky.
 
 echo ""
 echo ""
